31a250533cba0b99e7816ae469e07a1dab94276e
mac <mac@MacBook-Pro-3.local>
2024-11-08 23:13:00 +0200

    rebuild structure using webpack

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import setupModal from "./components/setupModal";
+import slider from "./components/slider";
+import sidebar from "./components/sidebar";
+import setupMegaMenu from "./components/setupMegaMenu";
+import phoneInput from "./components/phoneInput";
+
+document.addEventListener("DOMContentLoaded", function() {
+    setupModal();
+    slider();
+    sidebar();
+    setupMegaMenu();
+    phoneInput();
+});
